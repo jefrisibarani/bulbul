a7c6f23f64c0b3051b067165b0719bb9798b8541
Jefri Sibarani <jefrisibarani@gmail.com>
2021-09-21 01:38:28 +0700

    Edit .gitignore

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $baseURL = 'https://alklin.local';
+	public $baseURL = 'https://devel.local';
 
 	/**
 	 * --------------------------------------------------------------------------
